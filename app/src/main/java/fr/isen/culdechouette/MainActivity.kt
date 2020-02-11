02e655385dd4e9ad08d8a97382d615b3f687c41d
Nico <nicolas.mengozzi@outlook.fr>
2020-02-11 16:03:46 +0100

    First layout completed

--- app/src/main/java/fr/isen/culdechouette/MainActivity.kt
+++ app/src/main/java/fr/isen/culdechouette/MainActivity.kt
@@ -1,7 +1,10 @@
 package fr.isen.culdechouette
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 //Creation of the main menu, nothing really interesting here
 
@@ -10,5 +13,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainID.setOnClickListener{ doLogin() }
     }
+
+    private fun doLogin() {
+        val intentHome = Intent( this@MainActivity, HostChoiceActivity::class.java)
+        intentHome.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NO_ANIMATION
+        this@MainActivity.startActivity(intentHome)
+    }
+
+
 }
