41571e768630d005bc8ff8502f95c1d6132f10af
Nico <nicolas.mengozzi@outlook.fr>
2020-02-17 08:48:50 +0100

    NavBar and TaskBar hidden

--- app/src/main/java/fr/isen/culdechouette/MainActivity.kt
+++ app/src/main/java/fr/isen/culdechouette/MainActivity.kt
@@ -1,10 +1,13 @@
 package fr.isen.culdechouette
 
+
+import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import kotlinx.android.synthetic.main.activity_main.*
+import android.view.WindowManager
+
 
 //Creation of the main menu, nothing really interesting here
 
@@ -13,7 +16,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        actionBar?.hide()
         mainID.setOnClickListener{ doLogin() }
+    }
+
+    override fun onResume () {
+        super.onResume()
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        actionBar?.hide()
     }
 
     private fun doLogin() {
--- app/src/main/java/fr/isen/culdechouette/TutorialActivity.kt
+++ app/src/main/java/fr/isen/culdechouette/TutorialActivity.kt
@@ -2,11 +2,23 @@ package fr.isen.culdechouette
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 
 class TutorialActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tutorial)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        actionBar?.hide()
+    }
+
+    override fun onResume () {
+        super.onResume()
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        actionBar?.hide()
     }
 }
